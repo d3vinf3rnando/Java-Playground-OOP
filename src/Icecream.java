00fe3e5ff282dd99542a1afda45e377050bd863e
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2022-12-21 19:32:45 +0530

    init java playground oop

---
+++ src/Icecream.java
@@ -0,0 +1,24 @@
+public class Icecream{
+
+    String name;
+    String flavor;
+    String toppings;
+    int price;
+
+    public void iceName(){
+        System.out.println("Ice Cream Name: "+name);
+    }
+
+    public void invoice(){
+        System.out.println(name);
+        System.out.println(flavor);
+        System.out.println(toppings);
+        System.out.println(price);
+    }
+
+    public static void main(String[]args){
+
+    }
+
+
+}
