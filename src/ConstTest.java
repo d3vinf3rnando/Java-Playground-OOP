3993a04b844f327b50079125ddef099f1e18c5b3
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2022-12-31 09:23:12 +0530

    updates

--- src/ConstTest.java
+++ src/ConstTest.java
@@ -10,5 +10,20 @@ public class ConstTest {
     public static void main(String[]args){
         ConstTest student1 = new ConstTest();
         System.out.println(student1.stuName);
+
+        int num = 0;
+        while(num<5){
+            System.out.print(num+" ");
+            num++;
+        }
+
+        System.out.println();
+
+        //lists java
+        String [] nameList={"nimal","kamal","sunil"};
+        System.out.println(nameList[0]);
+
+        int [][][] numbers={{{1,3,5},{2,4,6}},{{1,2,3,4,5,6}}};
+        System.out.println(numbers[1][0][1]);
     }
 }
--- src/Lottery.java
+++ src/Lottery.java
@@ -1,3 +1,18 @@
+import java.util.Random;
 public class Lottery {
+    public static void main(String[] args) {
+        Random random = new Random();
 
+        for (int i = 0; i < 6; i++) {
+            int randomNumber = random.nextInt(49) + 1;
+            System.out.println(randomNumber);
+
+
+            String Name="devicececn";
+            String Name1="devececbd   seucvbuececdcin";
+            String Name21="decdececfececcvdcvin";
+            String Name2="devvcdecedcfvfvin";
+
+        }
+    }
 }
