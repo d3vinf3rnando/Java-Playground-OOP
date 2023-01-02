f1c43cab85d79a02b973a1cb330ce28695bf6818
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2023-01-02 11:08:04 +0530

    company class updated

--- src/Company/Employee.java
+++ src/Company/Employee.java
@@ -4,6 +4,10 @@ public class Employee {
     String name; //instance varaibles
     int age;
 
+    private int empId;
+    private String empEmail;
+
+
     public void ageFinder(){
 
         String a="adult"; //local variables (example)
@@ -12,5 +16,22 @@ public class Employee {
         }else {
             System.out.println("child");
         }
+
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpEmail() {
+        return empEmail;
+    }
+
+    public void setEmpEmail(String empEmail) {
+        this.empEmail = empEmail;
     }
 }
--- src/Company/Main.java
+++ src/Company/Main.java
@@ -5,6 +5,12 @@ public class Main {
     Employee E1 = new Employee();
     E1.name="sunil";
     E1.age=12;
+
+    E1.setEmpId(2001);
+    E1.setEmpEmail("test@gmail.com");
     E1.ageFinder();
+
+        System.out.println( E1.getEmpId());
+        System.out.println(E1.getEmpEmail());
     }
 }
